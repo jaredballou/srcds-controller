8d1f87589d69a6d41627fbe0946fe7c9acf9bf91
hannes.scharmann <hannes.scharmann@056cb51c-820f-11de-a95a-7be59a9e0318>
2009-08-08 21:28:39 +0000

    refactored xslt handler

--- srcds-controller/src/main/java/de/eqc/srcds/handlers/XsltHandler.java
+++ srcds-controller/src/main/java/de/eqc/srcds/handlers/XsltHandler.java
@@ -2,10 +2,12 @@ package de.eqc.srcds.handlers;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.InetAddress;
 import java.net.URL;
 
 import com.sun.net.httpserver.HttpExchange;
 
+import de.eqc.srcds.handlers.utils.SimpleTemplate;
 import de.eqc.srcds.xmlbeans.enums.ResponseCode;
 import de.eqc.srcds.xmlbeans.impl.ControllerResponse;
 import de.eqc.srcds.xmlbeans.impl.Message;
@@ -23,33 +25,14 @@ public class XsltHandler extends AbstractRegisteredHandler implements
      */
     @Override
     public void handleRequest(HttpExchange httpExchange) throws IOException {
-	StringBuilder builder = new StringBuilder();
 
-	String beanName = getParameter("bean");	
-	URL resourceUrl = getClass().getResource(String.format("/xslt/%s.xsl", beanName));
-	if (resourceUrl == null) {
-	    if (beanName == null) {
-		throw new IllegalArgumentException("Parameter specify the parameter 'bean'");
-	    } else {
-		throw new IllegalArgumentException(String.format("Cannot find XSLT for name %s", beanName));
-	    }
-	} else {
-        	InputStream input = null;
-        	try {
-        	    input = resourceUrl.openStream();
-        
-        	    byte[] buffer = new byte[1024];
-        	    for (int len = 0; (len = input.read(buffer)) != -1;) {
-        		builder.append(new String(buffer, 0, len));
-        	    }
-        	} finally {
-        	    if (input != null) {
-        		input.close();
-        	    }
-        	}
-	}
+	String beanName = getParameter("bean");
+	String resource = String.format("/xslt/%s.xsl", beanName);
+	
+	SimpleTemplate template = new SimpleTemplate(resource);
+	template.setAttribute("hostname", InetAddress.getLocalHost().getHostName());
 
-	outputXmlContent(builder.toString());
+	outputXmlContent(template.renderTemplate());
     }
 
     /*
