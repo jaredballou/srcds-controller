ea4ebfab331180aeff908d2094d3802fd9329b0d
hannes.scharmann <hannes.scharmann@056cb51c-820f-11de-a95a-7be59a9e0318>
2009-08-08 14:45:47 +0000

    did some minor changes to message bean modified setconfig handler to output xml

--- trunk/srcds-controller/src/main/java/de/eqc/srcds/xmlbeans/impl/Message.java
+++ trunk/srcds-controller/src/main/java/de/eqc/srcds/xmlbeans/impl/Message.java
@@ -1,6 +1,7 @@
 package de.eqc.srcds.xmlbeans.impl;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 import de.eqc.srcds.xmlbeans.XmlBean;
@@ -14,6 +15,11 @@ public class Message extends XmlBean {
     private static final long serialVersionUID = -3453607805613176580L;
     private final List<String> items;
     
+    public Message() {
+	
+	this.items = new LinkedList<String>();
+    }
+    
     public Message(String message) {
 
 	this(Arrays.asList(new String[] {message}));
@@ -24,13 +30,20 @@ public class Message extends XmlBean {
 	this.items = messages;
     }
 
+    public void addMessage(String message) {
+	
+	items.add(message);
+    }
+    
     @Override
     public String toXml(int indent) {
 
 	StringBuilder sb = new StringBuilder(header(indent));
 
-	if (items.size() == 1) {
-	    sb.append(indent(items.get(0), indent + 1));
+	if (items.size() == 0) {
+	    sb.append(indent("(empty message)\n", indent + 1));
+	} else if (items.size() == 1) {
+	    sb.append(indent(items.get(0) + "\n", indent + 1));
 	} else {
 	    for (String item : items) {
 		    sb.append(indent(String.format("<Item>%s</Item>\n", item), indent + 1));
