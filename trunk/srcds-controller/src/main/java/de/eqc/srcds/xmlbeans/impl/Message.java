f1d5cb4223bdfc939e5ac4a1cf06cb009e35d738
hannes.scharmann <hannes.scharmann@056cb51c-820f-11de-a95a-7be59a9e0318>
2009-08-08 15:44:35 +0000

    changed single line messages to output as single item xml element instead of value-only

--- trunk/srcds-controller/src/main/java/de/eqc/srcds/xmlbeans/impl/Message.java
+++ trunk/srcds-controller/src/main/java/de/eqc/srcds/xmlbeans/impl/Message.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import de.eqc.srcds.xmlbeans.XmlBean;
 
-
 public class Message extends XmlBean {
 
     /**
@@ -14,14 +13,14 @@ public class Message extends XmlBean {
      */
     private static final long serialVersionUID = -3453607805613176580L;
     private final List<String> items;
-    
+
     public Message() {
-	
+
 	super(false);
 	this.items = new LinkedList<String>();
     }
 
-    public Message(String ... messages) {
+    public Message(String... messages) {
 
 	super(false);
 	if (messages == null) {
@@ -32,36 +31,35 @@ public class Message extends XmlBean {
     }
 
     public void addMessage(String message) {
-	
+
 	items.add(message);
     }
-    
+
     @Override
     protected String toXml(int indent) {
 
 	StringBuilder sb = new StringBuilder(header(indent));
 
 	if (items.size() == 0) {
-	    sb.append(indent("(empty message)\n", indent + 1));
-	} else if (items.size() == 1) {
-	    sb.append(indent(items.get(0) + "\n", indent + 1));
-	} else {
-	    for (String message : items) {
-		    sb.append(indent(String.format("<Item>%s</Item>\n", message), indent + 1));
-	    }
+	    addMessage("(no message specified)");
+	}
+
+	for (String message : items) {
+	    sb.append(indent(String.format("<Item>%s</Item>\n", message),
+		    indent + 1));
 	}
 
 	return sb.append(footer(indent)).toString();
     }
-    
+
     @Override
     public String toString() {
-    
+
 	StringBuilder sb = new StringBuilder();
-        for (String message : items) {
-            sb.append(message + "\n");
-        }
-        return sb.toString();
+	for (String message : items) {
+	    sb.append(message + "\n");
+	}
+	return sb.toString();
     }
 
 }
