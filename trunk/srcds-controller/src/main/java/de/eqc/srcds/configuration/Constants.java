3b6c7f4ff28c646c49e86029668a07f2f748e88b
hannes.scharmann <hannes.scharmann@056cb51c-820f-11de-a95a-7be59a9e0318>
2009-08-08 16:15:54 +0000

    added http server poll interval

--- trunk/srcds-controller/src/main/java/de/eqc/srcds/configuration/Constants.java
+++ trunk/srcds-controller/src/main/java/de/eqc/srcds/configuration/Constants.java
@@ -4,24 +4,27 @@ public final class Constants {
 
     /** Hides the constructor of the utility class. */
     private Constants() {
-        throw new UnsupportedOperationException();
+
+	throw new UnsupportedOperationException();
     }
-    
+
     public static final int STARTUP_WAIT_TIME_MILLIS = 2000;
-    
+
+    public static final int HTTP_SERVER_POLL_TIME_MILLIS = 1000;
+
     public static final String LOGGING_FILENAME = "srcds-controller-logging.properties";
-    
+
     public static final String DEFAULT_CONFIG_FILENAME = "srcds-controller-config.xml";
-    
-	public static final String HTTP_SERVER_PORT = "srcds.controller.networking.httpserver.port";
 
-	public static final String AUTOSTART = "srcds.controller.autostart";
+    public static final String HTTP_SERVER_PORT = "srcds.controller.networking.httpserver.port";
 
-	public static final String SRCDS_PATH = "srcds.controller.srcds.path";
+    public static final String AUTOSTART = "srcds.controller.autostart";
 
-	public static final String SRCDS_EXECUTABLE = "srcds.controller.srcds.executable";
+    public static final String SRCDS_PATH = "srcds.controller.srcds.path";
 
-	public static final String SRCDS_PARAMETERS = "srcds.controller.srcds.parameters";
-	
-	public static final String SRCDS_GAMETYPE = "srcds.controller.srcds.gametype";
+    public static final String SRCDS_EXECUTABLE = "srcds.controller.srcds.executable";
+
+    public static final String SRCDS_PARAMETERS = "srcds.controller.srcds.parameters";
+
+    public static final String SRCDS_GAMETYPE = "srcds.controller.srcds.gametype";
 }
