18a2b417e573f10ab3d99d88ee6e5ab4285e26f2
hannes.scharmann <hannes.scharmann@056cb51c-820f-11de-a95a-7be59a9e0318>
2009-08-09 21:00:49 +0000

    added server output reader for streams

---
+++ srcds-controller/src/main/java/de/eqc/srcds/core/ServerOutputReader.java
@@ -0,0 +1,57 @@
+package de.eqc.srcds.core;
+
+import static de.eqc.srcds.core.Constants.SERVER_POLL_INTERVAL_MILLIS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Logger;
+
+import de.eqc.srcds.core.logging.LogFactory;
+
+public class ServerOutputReader extends Thread {
+
+    private static Logger log = LogFactory.getLogger(ServerOutputReader.class);
+
+    private final InputStream inputStream;
+    private boolean running;
+
+    public ServerOutputReader(InputStream inputStream) {
+
+	this.running = false;
+	this.inputStream = inputStream;
+    }
+
+    @Override
+    public void run() {
+
+	BufferedReader br = new BufferedReader(new InputStreamReader(
+		inputStream));
+	running = true;
+	log.info("Reading server output started");
+	try {
+	    String line;	    
+	    while ((line = br.readLine()) != null && running) {
+		if (line.startsWith("players :")) {
+		    log.info(line.trim());
+		}
+	    }
+	} catch (IOException e) {
+	    log.info(String.format("Error while reading server output: %s", e.getLocalizedMessage()));
+	} finally {
+	    log.info("Reading server output stopped");
+	}
+    }
+
+    public void stopGraceful() {
+
+	running = false;
+    }
+
+    public boolean isRunning() {
+
+	return running;
+    }
+
+}
