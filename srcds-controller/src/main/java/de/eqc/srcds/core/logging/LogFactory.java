484a7e456aabbfeaeffa9c6d3ccbcfc731f949d1
hannes.scharmann <hannes.scharmann@056cb51c-820f-11de-a95a-7be59a9e0318>
2009-08-10 17:31:09 +0000

    moved built-in logging configuration file to resources folder added logging configuration fallback to built-in

--- srcds-controller/src/main/java/de/eqc/srcds/core/logging/LogFactory.java
+++ srcds-controller/src/main/java/de/eqc/srcds/core/logging/LogFactory.java
@@ -1,9 +1,11 @@
 package de.eqc.srcds.core.logging;
 
-import static de.eqc.srcds.core.Constants.LOGGING_FILENAME;
+import static de.eqc.srcds.core.Constants.BUILTIN_LOGGING_FILENAME;
+import static de.eqc.srcds.core.Constants.FS_LOGGING_FILENAME;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
@@ -11,23 +13,33 @@ import java.util.logging.Logger;
  * @author Holger Cremer
  */
 public class LogFactory {
-  
-  static {
 
-    File loggingConfig = new File(LOGGING_FILENAME);
-    if (loggingConfig.exists()) {
-      try {
-        LogManager.getLogManager().readConfiguration(new FileInputStream(loggingConfig));
-      } catch (Exception e) {
-        System.out.println("Unable to read logging configuration file - using default");
-      }
-    } else {
-      System.out.println("Unable to find logging configuration file - using default");
+    static {
+
+	File fsLoggingConfig = new File(FS_LOGGING_FILENAME);
+	InputStream builtinLoggingConfig = LogFactory.class.getResourceAsStream(BUILTIN_LOGGING_FILENAME);
+
+	if (fsLoggingConfig.exists()) {
+	    try {
+		LogManager.getLogManager().readConfiguration(
+			new FileInputStream(fsLoggingConfig));
+	    } catch (Exception e) {
+		System.out
+			.println("Unable to read logging configuration file - using built-in default");
+	    }
+	} else {
+	    System.out.println("Using builtin logging configuration file");
+	    try {
+		LogManager.getLogManager().readConfiguration(builtinLoggingConfig);
+	    } catch (Exception e) {
+		System.out
+			.println("Unable to built-in logging configuration file - using default");
+	    }
+	}
     }
-  }
-  
-  public static Logger getLogger(Class<?> clazz) {
 
-    return Logger.getLogger(clazz.getName());
-  }
+    public static Logger getLogger(Class<?> clazz) {
+
+	return Logger.getLogger(clazz.getName());
+    }
 }
