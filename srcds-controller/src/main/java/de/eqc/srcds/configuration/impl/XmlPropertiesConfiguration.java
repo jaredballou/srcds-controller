aa0fa90d72aeb2ebe9e43910ab26143d4e4817f6
hannes.scharmann <hannes.scharmann@056cb51c-820f-11de-a95a-7be59a9e0318>
2009-08-12 22:03:52 +0000

    implemented password encryption

--- srcds-controller/src/main/java/de/eqc/srcds/configuration/impl/XmlPropertiesConfiguration.java
+++ srcds-controller/src/main/java/de/eqc/srcds/configuration/impl/XmlPropertiesConfiguration.java
@@ -17,157 +17,211 @@ import java.util.logging.Logger;
 import de.eqc.srcds.configuration.Configuration;
 import de.eqc.srcds.configuration.ConfigurationKey;
 import de.eqc.srcds.configuration.ConfigurationRegistry;
+import de.eqc.srcds.configuration.datatypes.Password;
 import de.eqc.srcds.configuration.exceptions.ConfigurationException;
+import de.eqc.srcds.core.CryptoUtil;
 import de.eqc.srcds.core.SourceDServerController;
 import de.eqc.srcds.xmlbeans.impl.ControllerConfiguration;
 
 public class XmlPropertiesConfiguration implements Configuration {
 
-	private static Logger log = Logger.getLogger(SourceDServerController.class.getSimpleName());
-	private final File propertiesFile;
-	private Properties properties;
-	
-	public XmlPropertiesConfiguration(File propertiesFile) throws ConfigurationException {
+    private static Logger log = Logger.getLogger(SourceDServerController.class
+	    .getSimpleName());
+    private final File propertiesFile;
+    private Properties properties;
 
-		this.propertiesFile = propertiesFile;
-		
-		if (propertiesFile.exists()) {
-			loadConfiguration();
-		} else {
-			createDefaultConfiguration();
-		}
+    public XmlPropertiesConfiguration(File propertiesFile)
+	    throws ConfigurationException {
+
+	this.propertiesFile = propertiesFile;
+
+	if (propertiesFile.exists()) {
+	    loadConfiguration();
+	} else {
+	    createDefaultConfiguration();
 	}
-	
-	@SuppressWarnings("unchecked")
-	@Override
-	public <T> T getValue(String key, Class<T> dataType) throws ConfigurationException {
-		T value = null;
+    }
 
-		if (String.class.isAssignableFrom(dataType)) {
-			value = (T) properties.getProperty(key);
-		} else {
-			try {
-				Method conversionMethod = dataType.getDeclaredMethod("valueOf", String.class);
-				value = (T) conversionMethod.invoke(dataType, properties.getProperty(key));
-			} catch (Exception e) {
-				throw new ConfigurationException(String.format("Conversion to datatype %s failed for %s", dataType.getName(), key));
-			}
-		}
-		return value;
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> T getValue(String key, Class<T> dataType)
+	    throws ConfigurationException {
+
+	if (!ConfigurationRegistry.matchesDataType(key, dataType)) {
+	    throw new ConfigurationException(String.format(
+		    "Invalid data type for configuration key %s", key));
 	}
 
-	@Override
-	public <T> void setValue(String key, T value) throws ConfigurationException {
+	T value = null;
 
-	    if (isValidKey(key)) {
-		properties.setProperty(key, value.toString());
-		store();
-		log.finest(String.format("%s = %s", key, value));
-	    } else {
-		throw new ConfigurationException(String.format("Configuration key %s is not a valid key", key));
+	if (String.class.isAssignableFrom(dataType)) {
+	    value = (T) properties.getProperty(key);
+	} else {
+	    try {
+		Method conversionMethod = dataType.getDeclaredMethod("valueOf",
+			String.class);
+		value = (T) conversionMethod.invoke(dataType, properties
+			.getProperty(key));
+	    } catch (Exception e) {
+		throw new ConfigurationException(String.format(
+			"Conversion to datatype %s failed for %s", dataType
+				.getName(), key));
 	    }
 	}
+	return value;
+    }
 
-	@Override
-	public void removeValue(String key) throws ConfigurationException {
-	    
-	    properties.remove(key);
+    @Override
+    public <T> void setValue(String key, T value) throws ConfigurationException {
+
+//	if (!ConfigurationRegistry.matchesDataType(key, value.getClass())) {
+//	    throw new ConfigurationException(String.format(
+//		    "Invalid data type for configuration key %s", key));
+//	}
+
+	if (isValidKey(key)) {
+	    properties.setProperty(key, value.toString());
 	    store();
-	    log.finest(String.format("Removed key %s", key));
+	    log.finest(String.format("%s = %s", key, value));
+	} else {
+	    throw new ConfigurationException(String.format(
+		    "Configuration key %s is not a valid key", key));
 	}
-	
-	private void loadConfiguration() throws ConfigurationException {
-		
-		properties = new Properties();
-		
-		try {
-			FileInputStream fis = new FileInputStream(propertiesFile);
-			this.properties.loadFromXML(fis);
-			fis.close();
-			validateConfiguration();
-		} catch (InvalidPropertiesFormatException e) {
-			log.warning("Configuration file seems to be corrupted - creating default");
-			createDefaultConfiguration();
-		} catch (Exception e) {
-			throw new ConfigurationException("Unable to load configuration file");
-		}
-	}
+    }
 
-	private void validateConfiguration() throws ConfigurationException {
-	    
-	    List<Object> keysToRemove = new LinkedList<Object>();
-	    for (Entry<Object, Object> entry : properties.entrySet()) {
-		if (!isValidKey(entry.getKey().toString())) {
-		    keysToRemove.add(entry.getKey());
-		}
+    @Override
+    public void removeValue(String key) throws ConfigurationException {
+
+	properties.remove(key);
+	store();
+	log.finest(String.format("Removed key %s", key));
+    }
+
+    private void loadConfiguration() throws ConfigurationException {
+
+	properties = new Properties();
+
+	try {
+	    FileInputStream fis = new FileInputStream(propertiesFile);
+	    Properties decryptedProperties = new Properties();
+	    decryptedProperties.loadFromXML(fis);
+	    process(CryptoUtil.Action.DECRYPT, decryptedProperties, this.properties);
+	    fis.close();
+	    validateConfiguration();
+	} catch (InvalidPropertiesFormatException e) {
+	    log
+		    .warning("Configuration file seems to be corrupted - creating default");
+	    createDefaultConfiguration();
+	} catch (Exception e) {
+	    throw new ConfigurationException(
+		    "Unable to load configuration file");
+	}
+    }
+
+    private void validateConfiguration() throws ConfigurationException {
+
+	List<Object> keysToRemove = new LinkedList<Object>();
+	for (Entry<Object, Object> entry : properties.entrySet()) {
+	    if (!isValidKey(entry.getKey().toString())) {
+		keysToRemove.add(entry.getKey());
 	    }
-	    for (Object keyToRemove : keysToRemove) {
-		removeValue(keyToRemove.toString());
-		log.info(String.format("Configuration entry %s is not an allowed entry - thus removed", keyToRemove.toString()));
+	}
+	for (Object keyToRemove : keysToRemove) {
+	    removeValue(keyToRemove.toString());
+	    log
+		    .info(String
+			    .format(
+				    "Configuration entry %s is not an allowed entry - thus removed",
+				    keyToRemove.toString()));
+	}
+	for (ConfigurationKey<?> registryEntry : ConfigurationRegistry
+		.getEntries()) {
+	    if (!containsKey(registryEntry.getKey())) {
+		setValue(registryEntry.getKey(), registryEntry
+			.getDefaultValue());
+		log
+			.info(String
+				.format(
+					"Configuration entry %s is missing in configuration - thus added with default value",
+					registryEntry.getKey()));
 	    }
-	    for (ConfigurationKey<?> registryEntry : ConfigurationRegistry.getEntries()) {
-		if (!containsKey(registryEntry.getKey())) {
-		    setValue(registryEntry.getKey(), registryEntry.getDefaultValue());
-		    log.info(String.format("Configuration entry %s is missing in configuration - thus added with default value", registryEntry.getKey()));
-		}
+	}
+    }
+
+    private boolean isValidKey(String key) {
+
+	return ConfigurationRegistry.getEntryByKey(key) != null;
+    }
+
+    private boolean containsKey(String key) {
+
+	return properties.get(key) != null;
+    }
+
+    private void createDefaultConfiguration() throws ConfigurationException {
+
+	if (propertiesFile.exists()) {
+	    propertiesFile.delete();
+	}
+
+	properties = new Properties();
+	for (ConfigurationKey<?> registryEntry : ConfigurationRegistry
+		.getEntries()) {
+	    setValue(registryEntry.getKey(), registryEntry.getDefaultValue());
+	}
+
+	store();
+    }
+
+    private void store() throws ConfigurationException {
+
+	try {
+	    FileOutputStream fos = new FileOutputStream(propertiesFile);
+	    Properties encryptedProperties = new Properties();
+	    process(CryptoUtil.Action.ENCRYPT, properties, encryptedProperties);
+	    encryptedProperties.storeToXML(fos, null);
+	    fos.flush();
+	    fos.close();
+	} catch (IOException e) {
+	    throw new ConfigurationException(
+		    "Unable to store configuration to file");
+	}
+
+	log.finest("Configuration stored to file.");
+    }
+    
+    private static void process(CryptoUtil.Action action, Properties src, Properties target) {
+
+	for (Entry<Object, Object> entry : src.entrySet()) {
+	    ConfigurationKey<?> configKey = ConfigurationRegistry
+		    .getEntryByKey(entry.getKey().toString());
+	    
+	    String value = entry.getValue().toString();
+	    if (configKey.getDataType() == Password.class) {
+		value = CryptoUtil.process(action, value);
 	    }
-	}
-	
-	private boolean isValidKey(String key) {
-	    
-	    return ConfigurationRegistry.getEntryByKey(key) != null;
-	}
-	
-	private boolean containsKey(String key) {
-	    
-	    return properties.get(key) != null;
-	}
-	
-	private void createDefaultConfiguration() throws ConfigurationException {
-		
-		if (propertiesFile.exists()) {
-			propertiesFile.delete();
-		}
+	    target.put(configKey.getKey(), value);
+	}	
+    }    
+    
+    @Override
+    public String toXml() {
 
-		properties = new Properties();
-		for (ConfigurationKey<?> registryEntry : ConfigurationRegistry.getEntries()) {
-			setValue(registryEntry.getKey(), registryEntry.getDefaultValue());
-		}
-		
-		store();	
-	}
-	
-	private void store() throws ConfigurationException {
+	return new ControllerConfiguration(this).toXml();
+    }
 
-		try {
-			FileOutputStream fos = new FileOutputStream(propertiesFile);
-			properties.storeToXML(fos, null);
-			fos.flush();
-			fos.close();
-		} catch (IOException e) {
-			throw new ConfigurationException("Unable to store configuration to file");
-		}
-		
-		log.finest("Configuration stored to file.");
-	}
-	
-	@Override
-	public String toXml() {
-		
-		return new ControllerConfiguration(this).toXml();
+    @Override
+    public Map<ConfigurationKey<?>, String> getData() {
+
+	Map<ConfigurationKey<?>, String> data = new TreeMap<ConfigurationKey<?>, String>();
+
+	for (Entry<Object, Object> entry : properties.entrySet()) {
+	    ConfigurationKey<?> configKey = ConfigurationRegistry
+		    .getEntryByKey(entry.getKey().toString());
+	    data.put(configKey, entry.getValue().toString());
 	}
 
-	@Override
-	public Map<ConfigurationKey<?>, String> getData() {
-
-	    Map<ConfigurationKey<?>, String> data = new TreeMap<ConfigurationKey<?>, String>();
-	    
-	    for (Entry<Object, Object> entry : properties.entrySet()) {
-		ConfigurationKey<?> configKey = ConfigurationRegistry.getEntryByKey(entry.getKey().toString());
-		data.put(configKey, entry.getValue().toString());
-	    }
-	    
-	    return data;
-	}
+	return data;
+    }
 
 }
---
+++ srcds-controller/src/main/java/de/eqc/srcds/core/CryptoUtil.java
@@ -0,0 +1,67 @@
+package de.eqc.srcds.core;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+public final class CryptoUtil {
+
+    private final static String CRYPTO_KEY = "Aj17Ag%!&YJA!(.0";
+
+    public enum Action {
+	ENCRYPT, DECRYPT;
+    }
+
+    /** Hides the constructor of the utility class. */
+    private CryptoUtil() {
+
+	throw new UnsupportedOperationException();
+    }
+
+    public static String process(Action action, String text) {
+
+	String ret = null;
+	switch (action) {
+	case ENCRYPT:
+	    ret = encrypt(text);
+	    break;
+	case DECRYPT:
+	    ret = decrypt(text);
+	    break;
+	}
+	return ret;
+    }
+
+    public static String decrypt(String encoded) {
+
+	try {
+	    SecretKey secret = new SecretKeySpec(CRYPTO_KEY.getBytes("UTF-8"),
+		    "Blowfish");
+	    Cipher cipher = Cipher.getInstance("Blowfish/ECB/PKCS5Padding");
+	    cipher.init(Cipher.DECRYPT_MODE, secret);
+	    byte[] bytes = new BASE64Decoder().decodeBuffer(encoded);
+	    return new String(cipher.doFinal(bytes), "UTF-8");
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    throw new RuntimeException("Unable to decrypt password");
+	}
+    }
+
+    public static String encrypt(String plain) {
+
+	try {
+	    SecretKey secret = new SecretKeySpec(CRYPTO_KEY.getBytes("UTF-8"),
+		    "Blowfish");
+	    Cipher cipher = Cipher.getInstance("Blowfish/ECB/PKCS5Padding");
+	    cipher.init(Cipher.ENCRYPT_MODE, secret);
+	    byte[] bytes = plain.getBytes("UTF-8");
+	    return new BASE64Encoder().encode(cipher.doFinal(bytes));
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    throw new RuntimeException("Unable to encrypt password");
+	}
+    }
+}
