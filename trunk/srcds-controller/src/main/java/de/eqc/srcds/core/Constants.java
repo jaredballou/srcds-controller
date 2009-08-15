8c4dd95971b7678254f1c07df67b99113480c397
hannes.scharmann <hannes.scharmann@056cb51c-820f-11de-a95a-7be59a9e0318>
2009-08-15 23:39:14 +0000

    added license header to all files

--- trunk/srcds-controller/src/main/java/de/eqc/srcds/core/Constants.java
+++ trunk/srcds-controller/src/main/java/de/eqc/srcds/core/Constants.java
@@ -1,3 +1,25 @@
+/**
+ * This file is part of the Source Dedicated Server Controller project.
+ * It is distributed under GPL 3 license.
+ *
+ * The srcds-controller is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the srcds-controller. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more information, please consult:
+ *    <http://www.earthquake-clan.de/srcds/>
+ *    <http://code.google.com/p/srcds-controller/>
+ */
 package de.eqc.srcds.core;
 
 
