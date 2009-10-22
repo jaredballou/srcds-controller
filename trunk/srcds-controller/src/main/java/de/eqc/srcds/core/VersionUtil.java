f7b25f13aaf746a3520701de1d03be0213e585d7
hannes.scharmann <hannes.scharmann@056cb51c-820f-11de-a95a-7be59a9e0318>
2009-10-22 19:39:18 +0000

    fixed typo

--- trunk/srcds-controller/src/main/java/de/eqc/srcds/core/VersionUtil.java
+++ trunk/srcds-controller/src/main/java/de/eqc/srcds/core/VersionUtil.java
@@ -52,9 +52,9 @@ public final class VersionUtil {
 	    String pomPropertiesLocation = POM_PROPERTIES_LOCATION;
 	    pomPropertiesLocation = pomPropertiesLocation.replaceAll("\\$\\{root-package\\}", ROOT_PACKAGE);
 	    pomPropertiesLocation = pomPropertiesLocation.replaceAll("\\$\\{project-name\\}", PROJECT_NAME);
-	    final InputStream intputStream = VersionUtil.class.getResourceAsStream(pomPropertiesLocation);
+	    final InputStream inputStream = VersionUtil.class.getResourceAsStream(pomPropertiesLocation);
 	    final Properties props = new Properties();
-	    props.load(intputStream);
+	    props.load(inputStream);
 	    projectVersion = props.getProperty("version");
 	} catch (Exception e) {
 	    projectVersion = "(unknown)";
